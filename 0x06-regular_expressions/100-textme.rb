b81b4ce7f6866993fbd350079a875c6516b8ef23
Ernest <gbaboh.ernest@gmail.com>
2023-10-04 00:42:14 +0000

    parses a log file for info about sender, receiver, and flag

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# This script parses a log file and output info about:
+#   [SENDER], [RECIEVER], [FLAGS]
+
+Format = ARGV[0].scan(/from:(\w+)|to:(\w+)|flags:([0-9:-]+)/)
+
+sender = Format[0].compact[0]
+receiver = Format[1].compact[0]
+flags = Format[2].compact[0]
+
+List - [sender, receiver, flags].compact
+puts List.join(',')
