e0028fe19aa2dddf8cdc1664a3b9400ca1e56558
Ernest <gbaboh.ernest@gmail.com>
2023-10-04 00:44:21 +0000

    textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,7 +2,7 @@
 # This script parses a log file and output info about:
 #   [SENDER], [RECIEVER], [FLAGS]
 
-Format = ARGV[0].scan(/from:(\w+)|to:(\w+)|flags:([0-9:-]+)/)
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
 
 sender = Format[0].compact[0]
 receiver = Format[1].compact[0]
