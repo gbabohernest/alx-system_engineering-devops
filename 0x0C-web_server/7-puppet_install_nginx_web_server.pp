308c587a7460d6ce6316da07e12784212084a0c1
Ernest <gbaboh.ernest@gmail.com>
2023-11-30 16:34:49 +0000

    modify the puppet manifest that install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,60 +1,22 @@
 # A puppet manifest that install Nginx web server
 
-# Install Nginx package
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
-# Nginx config
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => file,
-  content => "# Nginx server configuration
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm;
-
-    location / {
-        echo 'Hello World!';
-    }
-
-    location /redirect_me {
-        rewrite ^/redirect_me https://www.frontendmentor.io/profile/gbabohernest permanent;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/html;
-        internal;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server; ',
+  line   => 'rewrite ^/redirect_me https://www.frontendmentor.io/profile/gbabohernest permanent; ',
 }
 
-# Create the root directory for Nginx
-file { '/var/www/html':
-  ensure => directory,
+file { '/var/www/html/':
+  content => 'Hello World!',
 }
 
-# Create a custom 404 page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => 'Ceci n\'est pas une page',
-  require => File['/var/www/html'],
-}
-
-# Manage Nginx service
 service { 'nginx':
-  ensure => running,
-  enable => true,
-}
-
-# Restart Nginx service when configuration changes
-exec { 'nginx-restart':
-  command     => '/usr/sbin/service nginx restart',
-  path        => ['/usr/bin', '/usr/sbin'],
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-enabled/default'],
+  ensure  => running,
+  require => Package['nginx'],
 }
