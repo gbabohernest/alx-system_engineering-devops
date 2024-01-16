f1c77ed7c3383e1d00c499b4697d5464db304368
Ernest <gbaboh.ernest@gmail.com>
2024-01-16 16:33:10 +0000

    Queries the Reddit API, returns the number of subscribers for a given reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a function that quires the Reddit API
+    Returns:
+            the number of subscribers(not active users,
+                                      total subscribers)
+            for a given subreddit.
+            if subreddit is invalid, return 0
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API
+       returns the number of subscribers for a given reddit
+    """
+    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    headers = {"user-agent": "user"}
+
+    try:
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data.get("data").get("subscribers")
+            return subscribers
+
+        return 0
+
+    except Exception:
+        return 0
