71e6feeae95185055b9144f1f318162659f8d27c
Ernest <gbaboh.ernest@gmail.com>
2024-01-18 02:57:55 +0000

    fix bug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,8 @@
 # puppet code to fix wrong file extensions in the WordPress file 'web-setting.php'.
 
-exec { 'fix_apache_issue':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-setting.php",
-  path    => '/usr/local/bin/:bin/'
+$path_file = '/var/www/html/wp-settings.php'
+
+exec { 'replace_in_place':
+  command => "sed -i 's/phpp/php/g' ${path_file}",
+  path    => ['/bin','/usr/bin']
 }
