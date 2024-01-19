dbd6dad322da215326170392b366b6a1f770476d
Ernest <gbaboh.ernest@gmail.com>
2024-01-19 01:24:01 +0000

    Queries the Reddit API, and returns the number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,19 +13,29 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-       Queries the Reddit API
-       returns the number of subscribers for a given reddit
+        Queries the Reddit API
+        returns the number of subscribers for a given reddit
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0)"
-                             "Gecko/20100101 Firefox/97.0"}
+    if subreddit is not None or isinstance(subreddit, str):
 
-    response = requests.get(url, headers=headers,
-                            allow_redirects=False)
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0)"
+                                 "Gecko/20100101 Firefox/97.0"}
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get("data").get("subscribers")
-        return subscribers
+        try:
+            response = requests.get(url, headers=headers,
+                                    allow_redirects=False)
 
-    return 0
+            if response.status_code == 200:
+                data = response.json()
+                subscribers = data.get("data").get("subscribers")
+                return subscribers
+
+            elif response.status_code == 404:
+                return 0
+
+            else:
+                return 0
+
+        except Exception:
+            return 0
