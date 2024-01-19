b8727c94c934e30653d7f968d3fb741923330170
Ernest <gbaboh.ernest@gmail.com>
2024-01-19 02:59:51 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,20 +13,23 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-        Queries the Reddit API
-        returns the number of subscribers for a given reddit
-    """
-
+          Queries the Reddit API
+          returns the number of subscribers for a given reddit
+      """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:97.0)"
                              "Gecko/20100101 Firefox/97.0"}
 
-    response = requests.get(url, headers=header)
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        subs = response.json()["data"]["subscribers"]
+        data = response.json()
+
+        return data['data']['subscribers']
+    elif response.status_code == 404:
+        return 0
     else:
-        subs = 0
-    return subs
+        return 0
