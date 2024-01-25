7aad4ee226a1f68c930f23937b3696e97319d698
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-01-25 21:02:23 +0000

    A puppet manifest that performs the Ngninx limits fix and restart the Nginx service

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to fix Nginx limits and restart the service
+exec { 'debugging fix':
+  command => "sed -i 's|ULIMIT=\"-n 15\"|ULIMIT=\"-n 4096\"|' /etc/default/nginx",
+  path    => '/bin'
+} ~>
+service { 'nginx':
+  ensure => running
+}
